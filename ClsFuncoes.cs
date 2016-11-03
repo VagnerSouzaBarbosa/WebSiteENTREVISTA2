5a036d19606b3f0399e91eeedd839055002fb8fa
VagnerSouzaBarbosa <bhiros@hotmail.com>
2016-11-03 01:09:52 -0200

    Funções com o Banco

---
+++ ClsFuncoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for ClsFuncoes
+/// </summary>
+public class ClsFuncoes
+    
+{
+    public string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Vagner\Desktop\WebSiteENTREVISTA2\App_Data\BD_ENTREVISTA.mdf;Integrated Security=True";
+    
+    public DataSet AbrirTabela(string sqltxt)
+    {
+        SqlConnection cnx = new SqlConnection(conexao);
+        cnx.Open();
+        SqlDataAdapter adp = new SqlDataAdapter(sqltxt, cnx);
+        DataSet dst = new DataSet();
+        adp.Fill(dst);
+        return dst;
+        
+    }
+}
